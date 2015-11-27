bc428f483a15994eb659826b2f4035eee40a65b5
Laurent DAVID <atomikcircus87@hotmail.fr>
2015-11-27 16:34:39 +0100

    first exemple

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+        $('.img-holder').imageScroll({
+//            image: null,
+//            imageAttribute: 'image',
+//            container: $('body'),
+//            windowObject: $(window),
+//            speed:.2,
+//            coverRatio:.75,
+//            coverRatio:1,
+//            holderClass: 'imageHolder',
+//            imgClass: 'img-holder-img',
+//            holderMinHeight: 200,
+//            holderMaxHeight: null,
+//            extraHeight: 50,
+//            mediaWidth: 1600,
+//            mediaHeight: 900,
+//            parallax: true,
+//            touch: false
+        });
